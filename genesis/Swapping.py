26692e1191607c7057d44193fb9f2698b2bf5cca
pbtose <pbtose@wpi.edu>
2026-02-09 15:51:41 -0500

    you can enter coords into swapping.py to move

--- genesis/Swapping.py
+++ genesis/Swapping.py
@@ -29,6 +29,20 @@ finley = scene.add_entity(
     ),
 )
 
+def get_user_target():
+    print("\nEnter target coordinates (e.g., '0.3 0.0 0.5'):")
+    try:
+        user_input = input(">> ")
+        coords = [float(x) for x in user_input.split()]
+        if len(coords) != 3:
+            print("Invalid input! Please enter 3 numbers.")
+            return None
+        return np.array(coords)
+    except ValueError:
+        print("Invalid numbers!")
+        return None
+    
+
 scene.build()
 
 #    [1=Shoulder, 5=ArmLift, 8=Elbow, 11=HandSpin, 15=WristCurl]
@@ -38,34 +52,44 @@ standing_qpos = finley.get_qpos()
 # get the end-effector link
 end_effector = finley.get_link('wrist_left')
 
+
+
 # move to pre-grasp pose
-qpos = finley.inverse_kinematics(
-    link = end_effector,
-    pos  = np.array([0.30, -0.70, 0.75]), #xyz
-    quat = np.array([0, 1, 0, 0]),
-)
+while True:
+        
+    qpos = finley.inverse_kinematics(
+        link = end_effector,
+        pos  = get_user_target(), #xyz
+        quat = np.array([0, 0, 0, 0]),
+    )
+    
+    if qpos is None:
+        print("target out of reach")
+        continue
 
-final_qpos = standing_qpos.clone()
+    final_qpos = standing_qpos.clone()
 
-for i in left_arm_indices:
-    final_qpos[i] = qpos[i]
+    for i in left_arm_indices:
+        final_qpos[i] = qpos[i]
 
-path = finley.plan_path(
-    qpos_goal     = final_qpos,
-    num_waypoints = 200,
-)
+    path = finley.plan_path(
+        qpos_goal     = final_qpos,
+        num_waypoints = 200,
+    )
 
-# active_joints = [j for j in finley.joints if j.n_dofs > 0]
-# print("------ ROBOT JOINTS ------")
-# for i, joint in enumerate(active_joints):
-#     print(f"DOF Index {i}: {joint.name}")
-# print("--------------------------")
+    # active_joints = [j for j in finley.joints if j.n_dofs > 0]
+    # print("------ ROBOT JOINTS ------")
+    # for i, joint in enumerate(active_joints):
+    #     print(f"DOF Index {i}: {joint.name}")
+    # print("--------------------------")
 
-# execute the planned path
-for waypoint in path:
-    finley.control_dofs_position(waypoint)
-    scene.step()
+    # execute the planned path
+    for waypoint in path:
+        finley.control_dofs_position(waypoint)
+        scene.step()
 
-# allow robot to reach the last waypoint
-for i in range(100):
-    scene.step()
+    # allow robot to reach the last waypoint
+    for i in range(100):
+        scene.step()
+
+
