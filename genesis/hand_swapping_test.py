ea208da6284f9d5d322fc5db6a7dd23f9231ba33
cjcotoia26 <cjcotoia@wpi.edu>
2026-04-21 22:49:54 -0400

    made edits to take pictures of simulation setup

--- genesis/hand_swapping_test.py
+++ genesis/hand_swapping_test.py
@@ -26,30 +26,26 @@ class HandSwappingTest(RobotArmController):
             gs.morphs.URDF(
                 file=self.urdf_file,
                 pos=self.robot_pos,
-                quat=(0, 0, 0, 1),
+                quat=(1, 0, 0, 0),
                 fixed=True,
             )
         )
 
         # Optional swapping station geometry
         self.scene.add_entity(
-            gs.morphs.Box(
-                size=(0.6, 0.6, 0.05),
-                pos=(0.0, 0.4, 0.62),
-                collision=True,
-                fixed=True,
-            )
+            gs.morphs.URDF(
+                file = 'genesis/SwappingStation/urdf/SwappingStationURDF.urdf',
+                pos = (1.30, 0.6, 1.60),
+                euler = (90, 90, 0), #x red, y green, z blue
+                fixed = True,
+            ),
         )
-
-        # Dual test pieces
-        self.scene.add_entity(gs.morphs.Box(size=(0.02, 0.02, 0.04), pos=(0.15, 0.4, 0.64), collision=True))
-        self.scene.add_entity(gs.morphs.Box(size=(0.02, 0.02, 0.04), pos=(-0.15, 0.4, 0.64), collision=True))
 
         self.scene.build()
 
     def move_arm_ready(self):
-        right_xy = np.array([0.45, 0.5, 0.75])
-        left_xy = np.array([-0.45, 0.5, 0.75])
+        right_xy = np.array([-0.45, -0.25, 0.75])
+        left_xy = np.array([0.45, -0.25, 0.75])
 
         for ee_name, target in [("wrist_right", right_xy), ("wrist_left", left_xy)]:
             self.ee_name = ee_name
@@ -62,75 +58,75 @@ class HandSwappingTest(RobotArmController):
         self.ee_name = "wrist_right"
         self._setup_arm_joints()
 
-    def pick(self, position, ee_name, above_offset=0.12, num_waypoints=45):
-        self.ee_name = ee_name
-        self._setup_arm_joints()
+    # def pick(self, position, ee_name, above_offset=0.12, num_waypoints=45):
+    #     self.ee_name = ee_name
+    #     self._setup_arm_joints()
 
-        above = position.copy(); above[2] += above_offset
-        ik_above = self.move_to_ik_target(above)
-        if ik_above is None:
-            return False
+    #     above = position.copy(); above[2] += above_offset
+    #     ik_above = self.move_to_ik_target(above)
+    #     if ik_above is None:
+    #         return False
 
-        self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
-        self.open_gripper(force=0.6, duration_steps=30)
+    #     self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
+    #     self.open_gripper(force=0.6, duration_steps=30)
 
-        ik_down = self.move_to_ik_target(position)
-        if ik_down is None:
-            return False
+    #     ik_down = self.move_to_ik_target(position)
+    #     if ik_down is None:
+    #         return False
 
-        self.execute_trajectory(self.plan_trajectory(ik_down, num_waypoints=num_waypoints), pause_steps=20)
-        self.close_gripper(force=-0.6, duration_steps=30)
+    #     self.execute_trajectory(self.plan_trajectory(ik_down, num_waypoints=num_waypoints), pause_steps=20)
+    #     self.close_gripper(force=-0.6, duration_steps=30)
 
-        self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
-        return True
+    #     self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
+    #     return True
 
-    def place(self, position, ee_name, above_offset=0.12, num_waypoints=45):
-        self.ee_name = ee_name
-        self._setup_arm_joints()
+    # def place(self, position, ee_name, above_offset=0.12, num_waypoints=45):
+    #     self.ee_name = ee_name
+    #     self._setup_arm_joints()
 
-        above = position.copy(); above[2] += above_offset
-        ik_above = self.move_to_ik_target(above)
-        if ik_above is None:
-            return False
+    #     above = position.copy(); above[2] += above_offset
+    #     ik_above = self.move_to_ik_target(above)
+    #     if ik_above is None:
+    #         return False
 
-        self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
+    #     self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
 
-        ik_down = self.move_to_ik_target(position)
-        if ik_down is None:
-            return False
+    #     ik_down = self.move_to_ik_target(position)
+    #     if ik_down is None:
+    #         return False
 
-        self.execute_trajectory(self.plan_trajectory(ik_down, num_waypoints=num_waypoints), pause_steps=20)
-        self.open_gripper(force=0.6, duration_steps=30)
+    #     self.execute_trajectory(self.plan_trajectory(ik_down, num_waypoints=num_waypoints), pause_steps=20)
+    #     self.open_gripper(force=0.6, duration_steps=30)
 
-        self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
-        return True
+    #     self.execute_trajectory(self.plan_trajectory(ik_above, num_waypoints=num_waypoints), pause_steps=20)
+    #     return True
 
-    def swap(self, pos_a, pos_b, above_offset=0.12, num_waypoints=45):
-        self.move_arm_ready()
+    # def swap(self, pos_a, pos_b, above_offset=0.12, num_waypoints=45):
+    #     self.move_arm_ready()
 
-        if not self.pick(pos_a, ee_name="wrist_right", above_offset=above_offset, num_waypoints=num_waypoints):
-            print("Failed to pick piece A with right gripper")
-            return False
+    #     if not self.pick(pos_a, ee_name="wrist_right", above_offset=above_offset, num_waypoints=num_waypoints):
+    #         print("Failed to pick piece A with right gripper")
+    #         return False
 
-        if not self.pick(pos_b, ee_name="wrist_left", above_offset=above_offset, num_waypoints=num_waypoints):
-            print("Failed to pick piece B with left gripper")
-            return False
+    #     if not self.pick(pos_b, ee_name="wrist_left", above_offset=above_offset, num_waypoints=num_waypoints):
+    #         print("Failed to pick piece B with left gripper")
+    #         return False
 
-        if not self.place(pos_a, ee_name="wrist_left", above_offset=above_offset, num_waypoints=num_waypoints):
-            print("Failed to place piece B into A position")
-            return False
+    #     if not self.place(pos_a, ee_name="wrist_left", above_offset=above_offset, num_waypoints=num_waypoints):
+    #         print("Failed to place piece B into A position")
+    #         return False
 
-        if not self.place(pos_b, ee_name="wrist_right", above_offset=above_offset, num_waypoints=num_waypoints):
-            print("Failed to place piece A into B position")
-            return False
+    #     if not self.place(pos_b, ee_name="wrist_right", above_offset=above_offset, num_waypoints=num_waypoints):
+    #         print("Failed to place piece A into B position")
+    #         return False
 
-        self.move_arm_ready()
-        return True
+    #     self.move_arm_ready()
+    #     return True
 
 
 if __name__ == "__main__":
     controller = HandSwappingTest(
-        urdf_file="SwappingURDF//urdf//SwappingURDF.urdf",
+        urdf_file="genesis/SwappingURDF/urdf/SwappingURDF.urdf",
         ee_name="wrist_right",
         robot_pos=(0.0, 0.0, 0.735),
         show_viewer=True,
@@ -139,10 +135,10 @@ if __name__ == "__main__":
 
     controller.move_arm_ready()
 
-    pos_a = np.array([0.15, 0.35, 0.615])
-    pos_b = np.array([-0.15, 0.35, 0.615])
+    # pos_a = np.array([-0.15, -0.35, 0.615])
+    # pos_b = np.array([0.15, -0.35, 0.615])
 
-    success = controller.swap(pos_a, pos_b, above_offset=0.15, num_waypoints=60)
-    print("Hand swap test", "passed" if success else "failed")
+    # success = controller.swap(pos_a, pos_b, above_offset=0.15, num_waypoints=60)
+    # print("Hand swap test", "passed" if success else "failed")
 
-    controller.run_interactive_loop()
+    # controller.run_interactive_loop()
