01b1ce5a47910e494e950f55d9e574b11690bc47
CJStriker <calebjudestriker@gmail.com>
2026-09-24 17:54:09 -0400

    Added some rewards for standing up straight in Issac lab

---
+++ Locomotion/source/Balancing/Hardware/ava_bridge.py
@@ -0,0 +1,41 @@
+
+
+
+class ava_bridge:
+    def connect(self):
+        # connect to Ava
+        pass
+
+
+    def send_joint_positions(self, positions):
+        # Send positions to physical motors
+        pass
+
+    def read_joint_positions(self):
+        # Read encoder positions
+
+        pass
+
+    def read_joint_velocities(self):
+        # Read encoder velocities
+
+        pass
+
+    def read_imu(self):
+        # Read IMU
+
+        pass
+
+    def emergency_stop(self):
+        # Immediately disable motors
+
+        pass
+
+
+
+"""while simulation_running:
+    
+
+
+
+"""
